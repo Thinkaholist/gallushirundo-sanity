bb0070e620aa8aea8a4f2e4eabf0cefff7c0f1f1
Richárd Németh <nemethricsi@gmail.com>
2021-07-04 17:41:02 +0200

    fix description

--- schemas/objects/mainEvent.js
+++ schemas/objects/mainEvent.js
@@ -3,7 +3,7 @@ export default {
   name: 'mainEvent',
   type: 'object',
   description:
-    'The name and website of the main event where this concert will happen',
+    'The name and website of the main event where this concert will happen. Could be a Facebook event or a ticket selling site too',
   fields: [
     {
       title: 'Name',
