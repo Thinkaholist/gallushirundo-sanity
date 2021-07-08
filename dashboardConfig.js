7d00d740b5e511603f51b3064a57f2e010d10948
Richárd Németh <nemethricsi@gmail.com>
2021-07-08 11:26:29 +0200

    add netlify to sanity dashboard

--- dashboardConfig.js
+++ dashboardConfig.js
@@ -4,14 +4,19 @@ export default {
     //   name: 'sanity-tutorials',
     // },
     {
-      name: 'document-list',
+      name: 'netlify',
       options: {
-        title: 'Upcoming Events',
-        query: '*[_type == "event" && date >= $now] | order(date asc) [0...10]',
-      },
-      layout: {
-        width: 'medium',
-        height: 'auto',
+        title: 'A generált weboldal állapota a Netlify-on',
+        description:
+          'A weboldalad azért tud olyan gyors lenni, mert előre elkészül a szerveren (ún. build folyamat), statikus. Ha kész vagy a változtatásokkal, a "Deploy" gombbal szükséges egy új "oldalépítést" (build) generálnod. (Elég egyszer megnyomni a gombot). A Netlify badge sárgára fog változni (Building). Pár perc az egész! Ha a Netlify gomb ismét zöld lesz (Success), az oldalépítés sikeres volt, és a változtatásokat látnod kell a weboldaladon. Ha esetleg az építés sikertelen volt, a gomb pirosra változik(Failed), ebben az esetben semmilyen változás nem történik a weboldaladon, a régi verzió ugyanúgy él tovább. Szólj, és megnézzük, hol a hiba. A "view" link a weboldalra visz, az "admin"-ra kattintva pedig a Netlify build folyamatot lehet nyomon követni.',
+        sites: [
+          {
+            title: 'DEV Weboldal',
+            apiId: '1a716a27-3f48-4d3b-ac55-6666ebe5253a',
+            buildHookId: '60e6c08e71ca4bbe6409b90f',
+            name: 'gallus',
+          },
+        ],
       },
     },
     {
@@ -27,7 +32,11 @@ export default {
       },
     },
     {
-      name: 'project-users',
+      name: 'document-list',
+      options: {
+        title: 'Upcoming Events',
+        query: '*[_type == "event" && date >= $now] | order(date asc) [0...10]',
+      },
       layout: {
         width: 'small',
         height: 'auto',
@@ -35,8 +44,34 @@ export default {
     },
     {
       name: 'project-info',
+      options: {
+        data: [
+          {
+            title: 'Sanity repo',
+            value: 'https://github.com/Thinkaholist/gallushirundo-sanity',
+            category: 'Code',
+          },
+          {
+            title: 'Gatsby repo',
+            value: 'https://github.com/Thinkaholist/gallushirundo-frontend',
+            category: 'Code',
+          },
+          {
+            title: 'Frontend Deploy',
+            value: 'https://gallus.netlify.app/',
+            category: 'apps',
+          },
+        ],
+      },
       layout: {
         width: 'medium',
+        height: 'auto',
+      },
+    },
+    {
+      name: 'project-users',
+      layout: {
+        width: 'small',
         height: 'auto',
       },
     },
