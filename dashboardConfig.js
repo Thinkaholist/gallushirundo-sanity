6f82a668dc862a5c0b99aaf377897b951f7a1d61
Richárd Németh <nemethricsi@gmail.com>
2021-09-23 21:08:13 +0200

    edit dashboardconfig

--- dashboardConfig.js
+++ dashboardConfig.js
@@ -58,7 +58,7 @@ export default {
           },
           {
             title: 'Frontend Deploy',
-            value: 'https://gallus.netlify.app/',
+            value: 'https://gallushirundo.hu',
             category: 'apps',
           },
         ],
